9c46a87f7fe91c2af4787d905c428652591b3e86
achroscielewska <agnieszka.chroscielewska@altkomsoftware.pl>
2020-05-14 12:21:40 +0200

    vue_instance

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
-new Vue({
-    el: '#app',
-    data: {
-        title: 'Hello World!'
+const vm1data = {
+    title: 'The VueJS Instance',
+    showParagraph: false
+}
+
+Vue.component('hello', {
+    template: '<h1>Hello!!!</h1>'
+})
+
+const vm1 = new Vue({
+    data: vm1data,
+    methods: {
+        show: function () {
+            this.showParagraph = true;
+            this.updateTitle('The VueJS Instance (Updated)');
+            this.$refs.myButton.innerText = 'TEST'
+        },
+        updateTitle: function (title) {
+            this.title = title;
+        }
     },
     computed: {
+        lowercaseTitle: function () {
+            return this.title.toLowerCase();
+        }
     },
     watch: {
-
-    },
-    methods: {
-
+        title: function (value) {
+            alert('Title changed, new value: ' + value);
+        }
     }
 });
+
+vm1.$mount('#app1')
+
+setTimeout(function () {
+    vm1.title = "Change by Timer";
+}, 5000)
+console.log(vm1)
+
+const vm2 = new Vue({
+    el: '#app2',
+    data: {
+        title: 'The VueJS Instance 2'
+    },
+    methods: {
+        changeTitle: function () {
+            vm1.title = "Change by vm2";
+        }
+    }
+})
+
+const vm3 = new Vue({
+    template: '<h1>Hello</h1>'
+})
+
+vm3.$mount('#app3')
